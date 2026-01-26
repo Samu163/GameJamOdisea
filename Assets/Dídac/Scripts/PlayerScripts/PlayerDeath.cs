e404cec0ed5fb00f5d6da7b6da65f3cddc84c958
D1dii <didacgp123@gmail.com>
2026-01-26 18:36:21 +0100

    Death Explosion

--- Assets/Dídac/Scripts/PlayerScripts/PlayerDeath.cs
+++ Assets/Dídac/Scripts/PlayerScripts/PlayerDeath.cs
@@ -1,17 +1,26 @@
+using System.Collections;
 using UnityEngine;
 
 public class PlayerDeath : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    public GameObject rbHead;
+    public GameObject rbBody;
+    public GameObject rbBottom;
+    public LayerMask deathLayers;
+
+    private void ExplodePlayer()
     {
-        
+        rbHead.AddComponent<Rigidbody>();
+        rbHead.transform.parent = null;
+        rbHead.GetComponent<Rigidbody>().AddForce(Vector3.up * 2f, ForceMode.Impulse);
+        rbBody.AddComponent<Rigidbody>();
+        rbBody.transform.parent = null;
+        rbBody.GetComponent<Rigidbody>().AddForce(Vector3.right * 1f, ForceMode.Impulse);
+        rbBottom.AddComponent<Rigidbody>();
+        rbBottom.transform.parent = null;
+        rbBottom.GetComponent<Rigidbody>().AddForce(Vector3.left * 1.5f, ForceMode.Impulse);
+        GetComponent<Collider>().excludeLayers = deathLayers;
     }
 
     private void OnCollisionEnter(Collision collision)
@@ -19,7 +28,8 @@ public class PlayerDeath : MonoBehaviour
         if (collision.gameObject.CompareTag("Death"))
         {
             Debug.Log("Player has died collision.");
-            GameManager.instance.ResetLevel();
+            StartCoroutine(RespawnPlayer());
+            ExplodePlayer();
         }
     }
 
@@ -28,7 +38,14 @@ public class PlayerDeath : MonoBehaviour
         if (other.CompareTag("Death"))
         {
             Debug.Log("Player has died trigger.");
-            GameManager.instance.ResetLevel();
+            StartCoroutine(RespawnPlayer());
+            ExplodePlayer();
         }
+    }
+
+    private IEnumerator RespawnPlayer()
+    {
+        yield return new WaitForSeconds(1f);
+        GameManager.instance.ResetLevel();
     }
 }
