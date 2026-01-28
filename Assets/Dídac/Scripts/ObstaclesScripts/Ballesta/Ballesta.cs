4675023732115f4c32c76190c9d128e2c14c9ee8
Samu163 <125267861+Samu163@users.noreply.github.com>
2026-01-29 00:03:25 +0100

    ballesta y polish level 7

--- Assets/Dídac/Scripts/ObstaclesScripts/Ballesta/Ballesta.cs
+++ Assets/Dídac/Scripts/ObstaclesScripts/Ballesta/Ballesta.cs
@@ -3,40 +3,51 @@ using UnityEngine;
 
 public class Ballesta : MonoBehaviour
 {
+    [Header("Configuración")]
     [SerializeField] private float delayBeforeShoot = 0.5f;
     [SerializeField] private float shootInterval = 2.5f;
-    private bool canShoot = true;
+
+    [Header("Referencias")]
     public BallestaProjectile projectile;
     [SerializeField] private Transform shootingPosition;
     public LayerMask playerLayer;
 
-    // Update is called once per frame
+    // 1. Añadimos la referencia al Animator
+    [SerializeField] private Animator animator;
+
+    private bool canShoot = true;
+
     void Update()
     {
         if (canShoot)
         {
             Physics.Raycast(shootingPosition.position, transform.forward, out RaycastHit hitInfo, Mathf.Infinity, playerLayer);
             Debug.DrawRay(shootingPosition.position, transform.forward * 20f, Color.red);
+
             if (hitInfo.collider != null && (hitInfo.collider.CompareTag("Player")))
             {
-                StartCoroutine(Shoot(delayBeforeShoot));
+                StartCoroutine(Shoot(delayBeforeShoot));            
                 canShoot = false;
             }
         }
-        
     }
 
     private IEnumerator Shoot(float delay)
     {
         yield return new WaitForSeconds(delay);
+
+        // 2. Activamos la animación de disparo
         projectile.Shoot();
+        if (animator != null) animator.SetTrigger("Shoot");
         AudioManager.instance.PlayBallesta();
+
         StartCoroutine(EnableShootingAfterDelay(shootInterval));
     }
 
     private IEnumerator EnableShootingAfterDelay(float delay)
     {
         yield return new WaitForSeconds(delay);
+
         canShoot = true;
         projectile.ResetProjectile();
     }
